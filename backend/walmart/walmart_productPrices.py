38801be6c9f518e2aad4534c8420296c0e2c44d3
GowthamChowta <gautigadu091@gmail.com>
2022-10-22 17:04:09 -0400

    add walmart api

---
+++ backend/walmart/walmart_productPrices.py
@@ -0,0 +1,60 @@
+from serpapi import GoogleSearch
+import pgeocode
+from geopy.distance import geodesic
+
+params = {
+    "api_key": "cb4579e10bb941b61ca774c9088b4e831df7e9714d917a36c301e5cc132e0e60",
+    "device": "desktop",
+    "engine": "walmart",
+    "query": "eggs 18",
+    "store_id": "1991",
+}
+
+
+class Walmart:
+    def __init__(self):
+        self.walmartStoreData = pd.read_csv("./walmartStoreData.csv")
+        self.dist = pgeocode.Nominatim("us")
+        self.params = {
+            "api_key": "cb4579e10bb941b61ca774c9088b4e831df7e9714d917a36c301e5cc132e0e60",
+            "device": "desktop",
+            "engine": "walmart",
+        }
+
+    def getNearestStoreId(self, userLocation):
+        nearestStoreId = -1
+        nearestDistance = float("inf")
+        userData = self.dist.query_postal_code(userLocation)
+        userLat = userData.latitude
+        userLon = userData.longitude
+
+        for store in self.walmartStoreData.iterrows():
+
+            storeLon, storeLat, storeId, storeName, storePostalCode = store[1]
+            if userLocation[0:2] == str(storePostalCode)[0:2]:
+                curDistance = geodesic((storeLat, storeLon), (userLat, userLon)).miles
+                if curDistance < nearestDistance:
+                    nearestDistance = curDistance
+                    nearestStoreId = storeId
+        return nearestStoreId
+
+    def getProductsInNearByStore(self, product, zipcode):
+        self.params["query"] = product
+        self.params["store_id"] = self.getNearestStoreId(zipcode)
+        out = GoogleSearch(self.params).get_dictionary()
+        response = []
+        for item in out["organic_results"]:
+            response.append(
+                {
+                    "us_item_id": item["us_item_id"],
+                    "title": item["title"],
+                    "price": item["primary_offer"]["offer_price"],
+                }
+            )
+        return response
+
+
+# starter script
+# w = Walmart()
+# out = w.getProductsInNearByStore("eggs","47408")
+# out[:2]
