2c0dcd938c2ffd93012e7279a6d5115448ca2b35
Zagade, Manas Nitin <mzagade@iu.edu>
2022-12-02 16:23:31 -0500

    Tests modified according to new function signatures

--- backend/tests/unit tests/test_target.py
+++ backend/tests/unit tests/test_target.py
@@ -1,10 +1,8 @@
 
-import pytest
 from Retailers.target.getProductInfo import Target
-
-
 TEST_ZIPCODE = "47401"
 TEST_PRODUCT = "milk"
+LAT,LONG =None,None
 
 ##@pytest.mark.skip(reason="skipping to save api requests")
 
@@ -13,20 +11,19 @@ def test_target_store_endpoint():
     target = Target()
     
 
-    response = target.getNearestStoreId(TEST_ZIPCODE)
+    response = target.getNearestStoreId(TEST_ZIPCODE,LAT,LONG)
 
     assert int(response) !=  -1
 
     pass
 
 
-#@pytest.mark.skip(reason="skipping to save api requests")
 
 def test_target_product_endpoint():
 
     target = Target()
 
-    response = target.getProductsInNearByStore(TEST_PRODUCT,TEST_ZIPCODE)
+    response = target.getProductsInNearByStore(TEST_PRODUCT,TEST_ZIPCODE,LAT,LONG)
     
     assert type(response) == list
     assert len(response) != 0
--- backend/tests/unit tests/test_walmart.py
+++ backend/tests/unit tests/test_walmart.py
@@ -4,6 +4,9 @@ from Retailers.walmart.getProductinfo import Walmart
 
 TEST_ZIPCODE = "47401"
 TEST_PRODUCT = "milk"
+LAT,LONG =None,None
+
+
 #@pytest.mark.skip(reason="skipping to save api requests")
 
 def test_walmart_store_endpoint():
@@ -11,7 +14,7 @@ def test_walmart_store_endpoint():
     walmart = Walmart()
     
 
-    response = walmart.getNearestStoreId(TEST_ZIPCODE)
+    response = walmart.getNearestStoreId(TEST_ZIPCODE,LAT,LONG)
 
     assert int(response) !=  -1
 
@@ -23,7 +26,7 @@ def test_walmart_product_endpoint():
 
     walmart = Walmart()
 
-    response = walmart.getProductsInNearByStore(TEST_PRODUCT,TEST_ZIPCODE)
+    response = walmart.getProductsInNearByStore(TEST_PRODUCT,TEST_ZIPCODE,LAT,LONG)
     
     assert type(response) == list
     assert len(response) != 0
